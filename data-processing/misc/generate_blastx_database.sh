d25a79bb4da98cebc5c825964c78af403de9521b
gabriellesandstedt <131995265+gabriellesandstedt@users.noreply.github.com>
2024-07-01 13:08:15 -0600

    Update generate_blastx_database.sh

--- data-processing/misc/generate_blastx_database.sh
+++ data-processing/misc/generate_blastx_database.sh
@@ -17,3 +17,20 @@ gunzip Brassica_napus.annotation_v5.pep.fa.gz
 makeblastdb -in /scratch/gds44474/MIMULUS/snps_parents_til/data/BLAST_dbs/brassicanapus/Brassica_napus.annotation_v5.pep.fa -dbtype prot -out brassicanapus
 blastx -query til_197megs_sequences.fasta -db /scratch/gds44474/MIMULUS/snps_parents_til/data/BLAST_dbs/brassicanapus  -evalue 1e-10 -max_target_seqs 1 -num_threads 4 -outfmt 6 -out til_197megs_sequences_brassica_napus.txt
 blastx -query til_197megs_sequences.fasta -db /scratch/gds44474/MIMULUS/snps_parents_til/data/BLAST_dbs/brassicanapus -evalue 1e-10 -max_target_seqs 1 -num_threads 4 -max_hsps 1 -outfmt 6 -out til_197megs_sequences_brassica_napus.txt
+
+
+
+# Download and prepare sequences
+wget https://www.genoscope.cns.fr/brassicanapus/data/Brassica_napus.annotation_v5.pep.fa.gz
+gunzip Brassica_napus.annotation_v5.pep.fa.gz
+
+# Make BLAST databases
+makeblastdb -in Brassica_napus.annotation_v5.pep.fa -dbtype prot -out brassicanapus
+makeblastdb -in genes_of_interest.fasta -dbtype nucl -out other_species
+
+# Perform BLAST searches
+blastx -query genes_of_interest.fasta -db brassicanapus -evalue 1e-10 -max_target_seqs 1 -num_threads 4 -outfmt 6 -out til_to_brassica.txt
+tblastn -query Brassica_napus.annotation_v5.pep.fa -db tilingii -evalue 1e-10 -max_target_seqs 1 -num_threads 4 -outfmt 6 -out brassica_to_til.txt
+
+# Run Python script to find reciprocal hits
+python find_reciprocal_hits.py
