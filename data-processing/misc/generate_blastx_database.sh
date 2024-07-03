a1948a2ea05e57bb6c9d2e776ffbe95ab7bfe035
gabriellesandstedt <131995265+gabriellesandstedt@users.noreply.github.com>
2024-07-03 09:57:18 -0600

    Update generate_blastx_database.sh

--- data-processing/misc/generate_blastx_database.sh
+++ data-processing/misc/generate_blastx_database.sh
@@ -4,15 +4,15 @@ gunzip /scratch/gds44474/MIMULUS/snps_parents_til/data/blastx_db/GCF_000504015.1
 makeblastdb -in /scratch/gds44474/MIMULUS/snps_parents_til/data/blastn_db/GCF_000504015.1_Mimgu1_0_protein.faa -dbtype prot
 blastx -query shared_34_MEGs.fasta -db /scratch/gds44474/MIMULUS/snps_parents_til/data/blastx_db/GCF_000504015.1_Mimgu1_0_protein.faa  -evalue 1e-10 -max_target_seqs 10 -out shared_34_MEGs_blastx.txt -num_threads 4 -outfmt 6 -max_hsps 1 
 
-
+wget 
 gunzip Araport11_pep_20220914_representative_gene_model.gz
-makeblastdb -in /scratch/gds44474/MIMULUS/snps_parents_til/data/tair_protein/Araport11_pep_20220914_representative_gene_model -dbtype prot
+makeblastdb -in /scratch/gds44474/MIMULUS/snps_parents_til/data/BLAST_dbs/Araport11_pep_20220914_representative_gene_model -dbtype prot
 blastx -query shared_34_MEGs.fasta -db /scratch/gds44474/MIMULUS/snps_parents_til/data/tair_protein/Araport11_pep_20220914_representative_gene_model  -evalue 1e-10 -max_target_seqs 1 -num_threads 4 -outfmt 6 -out shared_34_MEGs_tair.txt
 
 
 
 #https://www.genoscope.cns.fr/brassicanapus/data/
-wget https://www.genoscope.cns.fr/brassicanapus/data/Brassica_napus.annotation_v5.pep.fa.gz
+wget https://www.genoscope.cns.fr/brassicanapus/data/Brassica_napus_v4.1.chromosomes.fa.gz
 gunzip Brassica_napus.annotation_v5.pep.fa.gz
 makeblastdb -in /scratch/gds44474/MIMULUS/snps_parents_til/data/BLAST_dbs/brassicanapus/Brassica_napus.annotation_v5.pep.fa -dbtype prot -out brassicanapus
 blastx -query til_197megs_sequences.fasta -db /scratch/gds44474/MIMULUS/snps_parents_til/data/BLAST_dbs/brassicanapus  -evalue 1e-10 -max_target_seqs 1 -num_threads 4 -outfmt 6 -out til_197megs_sequences_brassica_napus.txt
@@ -25,12 +25,23 @@ wget https://www.genoscope.cns.fr/brassicanapus/data/Brassica_napus.annotation_v
 gunzip Brassica_napus.annotation_v5.pep.fa.gz
 
 # Make BLAST databases
-makeblastdb -in Brassica_napus.annotation_v5.pep.fa -dbtype prot -out brassicanapus
-makeblastdb -in genes_of_interest.fasta -dbtype nucl -out other_species
+makeblastdb -in Brassica_napus.annotation_v5.cds.fa -dbtype nucl -out brassicanapus
+makeblastdb -in genes_of_interest.fasta -dbtype nucl -out tilingii
 
 # Perform BLAST searches
-blastx -query genes_of_interest.fasta -db brassicanapus -evalue 1e-10 -max_target_seqs 1 -num_threads 4 -outfmt 6 -out til_to_brassica.txt
-tblastn -query Brassica_napus.annotation_v5.pep.fa -db tilingii -evalue 1e-10 -max_target_seqs 1 -num_threads 4 -outfmt 6 -out brassica_to_til.txt
+blastn -query genes_of_interest.fasta -db brassicanapus -evalue 1e-10 -max_target_seqs 1 -num_threads 4 -outfmt 6 -out til_to_brassica.txt
+blastn -query Brassica_napus.annotation_v5.cds.fa -db tilingii -evalue 1e-10 -max_target_seqs 1 -num_threads 4 -outfmt 6 -out brassica_to_til.txt
 
 # Run Python script to find reciprocal hits
 python find_reciprocal_hits.py
+
+
+blastx -query genes_of_interest.fasta -db /scratch/gds44474/MIMULUS/snps_parents_til/data/BLAST_dbs/Araport11_pep_20220914_representative_gene_model  -evalue 1e-10 -max_target_seqs 1 -num_threads 4 -outfmt 6 -out til_to_AT.txt
+tblastn -query /scratch/gds44474/MIMULUS/snps_parents_til/data/BLAST_dbs/Araport11_pep_20220914_representative_gene_model -db tilingii -evalue 1e-10 -max_target_seqs 1 -num_threads 4 -outfmt 6 -out AT_to_til.txt
+python find_reciprocal_hits.py > recip_til_AT.txt
+
+
+makeblastdb -in /scratch/gds44474/MIMULUS/snps_parents_til/data/BLAST_dbs/Crubella_183_v1.0.cds.fa -dbtype nucl -out capsella_rubella
+blastn -query genes_of_interest.fasta -db capsella_rubella -evalue 1e-10 -max_target_seqs 1 -num_threads 4 -outfmt 6 -out til_to_caprubella.txt
+blastn -query Crubella_183_v1.0.cds.fa -db tilingii -evalue 1e-10 -max_target_seqs 1 -num_threads 4 -outfmt 6 -out caprubella_to_til.txt
+python find_reciprocal_hits.py > recip_til_caprubella.txt
