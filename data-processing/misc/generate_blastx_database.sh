59db7e63352547e865be472586682ad030f615ca
gabriellesandstedt <131995265+gabriellesandstedt@users.noreply.github.com>
2024-07-11 17:36:53 -0600

    Update generate_blastx_database.sh

--- data-processing/misc/generate_blastx_database.sh
+++ data-processing/misc/generate_blastx_database.sh
@@ -33,15 +33,35 @@ blastn -query genes_of_interest.fasta -db brassicanapus -evalue 1e-10 -max_targe
 blastn -query Brassica_napus.annotation_v5.cds.fa -db tilingii -evalue 1e-10 -max_target_seqs 1 -num_threads 4 -outfmt 6 -out brassica_to_til.txt
 
 # Run Python script to find reciprocal hits
-python find_reciprocal_hits.py
+python find_recip_hits.py
 
 
 blastx -query genes_of_interest.fasta -db /scratch/gds44474/MIMULUS/snps_parents_til/data/BLAST_dbs/Araport11_pep_20220914_representative_gene_model  -evalue 1e-10 -max_target_seqs 1 -num_threads 4 -outfmt 6 -out til_to_AT.txt
 tblastn -query /scratch/gds44474/MIMULUS/snps_parents_til/data/BLAST_dbs/Araport11_pep_20220914_representative_gene_model -db tilingii -evalue 1e-10 -max_target_seqs 1 -num_threads 4 -outfmt 6 -out AT_to_til.txt
-python find_reciprocal_hits.py > recip_til_AT.txt
+python find_recip_hits.py > recip_til_AT.txt
 
 
 makeblastdb -in /scratch/gds44474/MIMULUS/snps_parents_til/data/BLAST_dbs/Crubella_183_v1.0.cds.fa -dbtype nucl -out capsella_rubella
 blastn -query genes_of_interest.fasta -db capsella_rubella -evalue 1e-10 -max_target_seqs 1 -num_threads 4 -outfmt 6 -out til_to_caprubella.txt
 blastn -query Crubella_183_v1.0.cds.fa -db tilingii -evalue 1e-10 -max_target_seqs 1 -num_threads 4 -outfmt 6 -out caprubella_to_til.txt
-python find_reciprocal_hits.py > recip_til_caprubella.txt
+python find_recip_hits.py > recip_til_caprubella.txt
+
+
+makeblastdb -in Alyrata_384_v2.1.cds.fa -dbtype nucl -out arabidopsis_lyrata
+blastn -query genes_of_interest.fasta -db arabidopsis_lyrata -evalue 1e-10 -max_target_seqs 1 -num_threads 4 -outfmt 6 -out til_to_alyrata.txt
+blastn -query Alyrata_384_v2.1.cds.fa -db tilingii -evalue 1e-10 -max_target_seqs 1 -num_threads 4 -outfmt 6 -out alyrata_to_til.txt
+python find_recip_hits.py > recip_til_alyrata.txt
+
+makeblastdb -in ZmB73_5a.59_working.cds.fasta -dbtype nucl -out zea_mays
+blastn -query genes_of_interest.fasta -db zea_mays -evalue 1e-10 -max_target_seqs 1 -num_threads 4 -outfmt 6 -out til_to_zm.txt
+blastn -query ZmB73_5a.59_working.cds.fasta -db tilingii -evalue 1e-10 -max_target_seqs 1 -num_threads 4 -outfmt 6 -out zm_to_til.txt
+python find_recip_hits.py > zm_til_recip_hits.txt
+
+
+makeblastdb -in Slycopersicum_225_cds.fa -dbtype nucl -out sol
+blastn -query genes_of_interest.fasta -db sol -evalue 1e-10 -max_target_seqs 1 -num_threads 4 -outfmt 6 -out til_to_sol.txt
+blastn -query Slycopersicum_225_cds.fa -db tilingii -evalue 1e-10 -max_target_seqs 1 -num_threads 4 -outfmt 6 -out sol_to_til.txt
+python find_recip_hits.py > sol_til_recip_hits.txt
+
+
+
