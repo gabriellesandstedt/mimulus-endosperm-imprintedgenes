41ff8ad644c3336e86a41f40f2fbfb3fdc40c70c
gabriellesandstedt <131995265+gabriellesandstedt@users.noreply.github.com>
2026-02-02 11:30:40 -0700

    Update 07_call_imprinting.sh

--- data-processing/RNAseq-endosperm/07_call_imprinting.sh
+++ data-processing/RNAseq-endosperm/07_call_imprinting.sh
@@ -9,76 +9,75 @@
 #SBATCH --error callimprinting.error
 
 
-# LVR is spA
-# SOP is spB
+# SOP is sp A
+# LVR is sp B
 
 
 bash call_imprinted.sh \
-   -s /scratch/gds44474/MIMULUS/RNAseq_endosperm_til/data/star_pass2 \
-   -1 LVR1xSOP12_1_STAR_LVR_v1_MD_Split_Q60.noSH.pp.bam \
-   -2 SOP12xLVR1_1_STAR_LVR_v1_MD_Split_Q60.noSH.pp.bam \
-   -S final_til_forimp.bed \
-   -G tilingii_reference.gtf \
+   -s /scratch/gds44474/MIMULUS/rna_seq_26/til_rnaseq \
+   -1 SOP12xLVR1_1_STAR_LVR_v1_MD_Split_Q60.noSH.pp.bam \ 
+   -2 LVR1xSOP12_1_STAR_LVR_v1_MD_Split_Q60.noSH.pp.bam \
+   -S diagnostic_snps_tilsp_SOP_LVR_tilref.bed \
+   -G Mtilingiivar_LVR_860_v1.1.gene_exons.gtf \
    -R 2 \
    -M 85 \
    -P 50 \
    -o outdir_lvrsop1
 
 bash call_imprinted.sh \
-   -s /scratch/gds44474/MIMULUS/RNAseq_endosperm_til/data/star_pass2 \
-   -1 LVR1xSOP12_2_STAR_LVR_v1_MD_Split_Q60.noSH.pp.bam \
-   -2 SOP12xLVR1_2_STAR_LVR_v1_MD_Split_Q60.noSH.pp.bam \
-   -S final_til_forimp.bed \
-   -G tilingii_reference.gtf \
+   -s /scratch/gds44474/MIMULUS/rna_seq_26/til_rnaseq \
+   -1 SOP12xLVR1_2_STAR_LVR_v1_MD_Split_Q60.noSH.pp.bam \ 
+   -2 LVR1xSOP12_2_STAR_LVR_v1_MD_Split_Q60.noSH.pp.bam \
+   -S diagnostic_snps_tilsp_SOP_LVR_tilref.bed \
+   -G Mtilingiivar_LVR_860_v1.1.gene_exons.gtf \
    -R 2 \
    -M 85 \
    -P 50 \
    -o outdir_lvrsop2
 
 bash call_imprinted.sh \
-   -s /scratch/gds44474/MIMULUS/RNAseq_endosperm_til/data/star_pass2 \
-   -1 LVR1xSOP12_3_STAR_LVR_v1_MD_Split_Q60.noSH.pp.bam \
-   -2 SOP12xLVR1_3_STAR_LVR_v1_MD_Split_Q60.noSH.pp.bam \
-   -S final_til_forimp.bed \
-   -G tilingii_reference.gtf \
+   -s /scratch/gds44474/MIMULUS/rna_seq_26/til_rnaseq \
+   -1 SOP12xLVR1_3_STAR_LVR_v1_MD_Split_Q60.noSH.pp.bam \ 
+   -2 LVR1xSOP12_3_STAR_LVR_v1_MD_Split_Q60.noSH.pp.bam \
+   -S diagnostic_snps_tilsp_SOP_LVR_tilref.bed \
+   -G Mtilingiivar_LVR_860_v1.1.gene_exons.gtf \
    -R 2 \
    -M 85 \
    -P 50 \
    -o outdir_lvrsop3
 
 
-# TWN is spA
-# UTC is spB
+# UTC is spA
+# TWN is spB
 
 bash call_imprinted.sh \
-   -s /scratch/gds44474/MIMULUS/RNAseq_endosperm_til/data/star_pass2 \
-   -1 TWN36xUTC1_1_STAR_LVR_v1_MD_Split_Q60.noSH.pp.bam \
-   -2 UTC1xTWN36_1_STAR_LVR_v1_MD_Split_Q60.noSH.pp.bam \
-   -S final_caes_forimp.bed \
-   -G tilingii_reference.gtf \
+   -s /scratch/gds44474/MIMULUS/rna_seq_26/til_rnaseq \
+   -1 UTC1xTWN36_1_STAR_LVR_v1_MD_Split_Q60.noSH.pp.bam \
+   -2 TWN36xUTC1_1_STAR_LVR_v1_MD_Split_Q60.noSH.pp.bam \ 
+   -S diagnostic_snps_caessp_UTC_TWN_tilref.bed \
+   -G Mtilingiivar_LVR_860_v1.1.gene_exons.gtf \
    -R 2 \
    -M 85 \
    -P 50 \
    -o outdir_twnutc1
 
 bash call_imprinted.sh \
-   -s /scratch/gds44474/MIMULUS/RNAseq_endosperm_til/data/star_pass2 \
-   -1 TWN36xUTC1_2_STAR_LVR_v1_MD_Split_Q60.noSH.pp.bam \
-   -2 UTC1xTWN36_2_STAR_LVR_v1_MD_Split_Q60.noSH.pp.bam \
-   -S final_caes_forimp.bed \
-   -G tilingii_reference.gtf \
+   -s /scratch/gds44474/MIMULUS/rna_seq_26/til_rnaseq \
+   -1 UTC1xTWN36_2_STAR_LVR_v1_MD_Split_Q60.noSH.pp.bam \
+   -2 TWN36xUTC1_2_STAR_LVR_v1_MD_Split_Q60.noSH.pp.bam \ 
+   -S diagnostic_snps_caessp_UTC_TWN_tilref.bed \
+   -G Mtilingiivar_LVR_860_v1.1.gene_exons.gtf \
    -R 2 \
    -M 85 \
    -P 50 \
    -o outdir_twnutc2
 
-
 bash call_imprinted.sh \
-   -s /scratch/gds44474/MIMULUS/RNAseq_endosperm_til/data/star_pass2 \
-   -1 TWN36xUTC1_3_STAR_LVR_v1_MD_Split_Q60.noSH.pp.bam \
-   -2 UTC1xTWN36_3_STAR_LVR_v1_MD_Split_Q60.noSH.pp.bam \
-   -S final_caes_forimp.bed \
-   -G tilingii_reference.gtf \
+   -s /scratch/gds44474/MIMULUS/rna_seq_26/til_rnaseq \
+   -1 UTC1xTWN36_3_STAR_LVR_v1_MD_Split_Q60.noSH.pp.bam \
+   -2 TWN36xUTC1_3_STAR_LVR_v1_MD_Split_Q60.noSH.pp.bam \ 
+   -S diagnostic_snps_caessp_UTC_TWN_tilref.bed \
+   -G Mtilingiivar_LVR_860_v1.1.gene_exons.gtf \
    -R 2 \
    -M 85 \
    -P 50 \
