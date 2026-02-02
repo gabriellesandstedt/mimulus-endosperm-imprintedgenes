4164b139deb89cd4da48178c768b876a4a10fcac
gabriellesandstedt <131995265+gabriellesandstedt@users.noreply.github.com>
2026-02-02 10:59:38 -0700

    Create 05-1_filt_bed.sh

---
+++ caespitosa/snp_parents/05-1_filt_bed.sh
@@ -0,0 +1,15 @@
+awk 'BEGIN{OFS="\t"}
+$11 ~ /^[01][\/|][01]/ && $12 ~ /^[01][\/|][01]/ {
+    split($11,a,":"); gtA=a[1]
+    split($12,b,":"); gtB=b[1]
+
+    if (gtA=="0/0" || gtA=="0|0") alleleA=$6
+    else if (gtA=="1/1" || gtA=="1|1") alleleA=$7
+    else next
+
+    if (gtB=="0/0" || gtB=="0|0") alleleB=$6
+    else if (gtB=="1/1" || gtB=="1|1") alleleB=$7
+    else next
+
+    print $1,$2,$3,alleleA">"alleleB
+}' til.bed > diagnostic_snps_tilsp_SOP_LVR_caesref.bed
